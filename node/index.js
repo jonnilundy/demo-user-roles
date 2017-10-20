ca9dc824dc77a8769461d4657b34860ab008fc9f
Nolan Chan <nolan.chan@liferay.com>
2017-10-19 18:29:14 -0700

    POST /login route decides where user goes

--- node/index.js
+++ node/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const wedeploy = require('wedeploy');
 const wedeployMiddleware = require('wedeploy-middleware');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+const auth = wedeploy.auth('auth-userroles.wedeploy.io').auth('e685224b-9431-4580-824d-1358954bbcca');
 
-app.use(express.static('public/assets/styles'));
-app.use(express.static('public/assets/scripts'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const adminMiddleware = wedeployMiddleware.auth({
   url: 'auth-userroles.wedeploy.io',
@@ -18,11 +20,59 @@ const freeMiddleware = wedeployMiddleware.auth({
 });
 
 app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/static/index.html'));
+})
+
+app.get('/login', function(req, res) {
 	res.sendFile(path.join(__dirname, 'public/static/index.html'));
+})
+
+app.get('/signup', function(req, res) {
+  if (req.query.cmd === 'fail') {
+    alert('Sign-up failed. Try another email.');
+  }
+
+  res.sendFile(path.join(__dirname, 'public/static/signup/index.html'));
+})
+
+app.post('/user', function(req, res) {
+  auth.createUser({
+    email: req.body.email,
+    name: req.body.name,
+    password: req.body.password,
+    supportedScopes: ['free']
+  })
+  .then(function() {
+    auth.signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then(function() {
+      res.redirect('/user?access_token=' + auth.currentUser.token);
+    })
+  })
+  .catch(function() {
+    res.redirect('/signup?cmd=fail');
+  })
+})
+
+app.post('/login', function(req, res) {
+  auth.signInWithEmailAndPassword(req.body.email, req.body.password)
+  .then(function() {
+    const currentUser = auth.currentUser;
+    if (currentUser.hasSupportedScopes('free')) {
+      res.redirect('/user?access_token=' + auth.currentUser.token);
+    } 
+    else if (currentUser.hasSupportedScopes('team')) {
+      res.redirect('/user?access_token=' + auth.currentUser.token);
+    }
+    else {
+      res.redirect('/login');  
+    }
+  })
+
 })
 
 app.get('/user', freeMiddleware, function(req, res) {
   console.log('User: ', res.locals.auth.currentUser);
+  res.sendFile(path.join(__dirname, 'public/static/user/index.html'));
 })
 
 app.get('/admin', adminMiddleware, function(req, res) {
