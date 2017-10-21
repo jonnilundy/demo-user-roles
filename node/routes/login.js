9ba165195915c1f2f930cedfbe0b582dbe16044e
Jonni Lundy <jonathan.lundy@liferay.com>
2017-10-21 15:46:43 -0700

    Mobile styling fixes

--- node/routes/login.js
+++ node/routes/login.js
@@ -3,13 +3,13 @@ import {readFileSync} from 'fs';
 
 /**
  * Login Route
- * @param  {Request} req 
+ * @param  {Request} req
  * @param  {Response} res
  * @param  {Function} next
  */
 export async function login(req, res, next) {
   const source = readFileSync('./pages/index.html').toString();
   const template = handlebars.compile(source);
-  const html = template({title: 'Welcome'});
+  const html = template({title: 'Sign In'});
   res.send(html);
 }
