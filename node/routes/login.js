9fd55801d68c54cb43442388262cd804b86a90ce
Jonni Lundy <jonathan.lundy@liferay.com>
2017-10-24 11:02:29 -0700

    Adds error messages

--- node/routes/login.js
+++ node/routes/login.js
@@ -10,6 +10,6 @@ import {readFileSync} from 'fs';
 export async function login(req, res, next) {
   const source = readFileSync('./pages/index.html').toString();
   const template = handlebars.compile(source);
-  const html = template({title: 'Sign In'});
+  const html = template({title: 'Sign In', fail: req.query.cmd});
   res.send(html);
 }
--- node/routes/signup.js
+++ node/routes/signup.js
@@ -3,13 +3,13 @@ import {readFileSync} from 'fs';
 
 /**
  * Sign Up Route
- * @param  {Request} req 
+ * @param  {Request} req
  * @param  {Response} res
  * @param  {Function} next
  */
 export async function signup(req, res, next) {
   const source = readFileSync('./pages/signup.html').toString();
   const template = handlebars.compile(source);
-  const html = template({title: 'Sign Up'});
+  const html = template({title: 'Sign Up', fail: req.query.cmd});
   res.send(html);
 }
