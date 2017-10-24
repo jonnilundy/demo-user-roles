d27b89d86353e54aa7dea09c05fe30703a31ec06
Jonni Lundy <jonathan.lundy@liferay.com>
2017-10-24 09:41:32 -0700

    Adds user/user.id urls to user list

--- node/components/all-users.js
+++ node/components/all-users.js
@@ -6,10 +6,10 @@
 export function handlebarsListHelper(users) {
   let userList = '';
   for (const user of users) {
-    userList = userList + '<div class="user-item">' +
+    userList = userList + '<a class="user-item" href="user/' + user.id + '">' +
       '<div class="name">' + user.name + '</div>' +
       '<div class="email">' + user.email + '</div>' +
-      '<div class="role">' + user.supportedScopes[0] + '</div>' + '</div>';
+      '<div class="role">' + user.supportedScopes[0] + '</div>' + '</a>';
   }
   return userList;
 }
