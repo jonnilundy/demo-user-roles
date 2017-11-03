2278e5d5a7ab4f54307a9c949c4cee9b46659960
Nolan Chan <nolan.chan@liferay.com>
2017-11-03 16:10:11 -0700

    Allow user to create admin on signup

--- node/routes/post-user.js
+++ node/routes/post-user.js
@@ -5,21 +5,27 @@ const auth = wedeploy.auth(config.authServiceUrl).auth(config.masterToken);
 
 /**
  * Post User Route
- * @param  {Request} req 
+ * @param  {Request} req
  * @param  {Response} res
  * @param  {Function} next
  */
 export async function postUser(req, res, next) {
+  const isAdmin = req.body.admin;
+  const supportedScopes = isAdmin ? ['admin', 'free'] : ['free'];
   try {
     await auth.createUser({
       email: req.body.email,
       name: req.body.name,
       password: req.body.password,
-      supportedScopes: ['free'],
+      supportedScopes,
     });
     await auth.signInWithEmailAndPassword(req.body.email, req.body.password);
     res.cookie('access_token', auth.currentUser.token);
-    res.redirect('/profile');
+    if (isAdmin) {
+      res.redirect('/admin');
+    } else {
+      res.redirect('/profile');
+    }
   } catch (error) {
     res.redirect('/signup?cmd=fail');
   }
